b04c8f7739b875e86220b8c16c583194823beae2
Alex Hatch <89677719+alex-hatch@users.noreply.github.com>
2021-11-03 13:07:13 -1000

    Add files via upload

---
+++ MazeGeneratorTest.java
@@ -0,0 +1,24 @@
+package cs146F21.hatch.project3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MazeGeneratorTest {
+
+    @Test
+    void displayDepthFirstSolution() {
+    }
+
+    @Test
+    void displayBreadthFirstSolution() {
+    }
+
+    @Test
+    void displayShortestPath() {
+    }
+
+    @Test
+    void displayMaze() {
+    }
+}
