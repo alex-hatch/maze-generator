905bad5099bcc50274aea3129ccb141ae50e93da
christyj390 <89554751+christyj390@users.noreply.github.com>
2021-11-12 21:26:46 -0800

    Update MazeGeneratorTest.java

--- MazeGeneratorTest.java
+++ MazeGeneratorTest.java
@@ -5,20 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MazeGeneratorTest {
-
+    // need to test a 4x4, 7x7, and 10x10 grid
+    MazeGenerator testing = new MazeGenerator(4);
+    MazeGenerator odd = new MazeGenerator(7);
+    MazeGenerator even = new MazeGenerator(10);
+    
     @Test
     void displayDepthFirstSolution() {
+        
     }
 
     @Test
     void displayBreadthFirstSolution() {
+        
     }
 
     @Test
     void displayShortestPath() {
+        
     }
 
     @Test
     void displayMaze() {
+        
     }
 }
